3608cd96839fcfd9ddc518308e142b6bffd0ed38
Matt D'Agostino <mdagostino175@gmail.com>
2023-08-08 23:16:32 +1000

    Fixing mobile issues

--- components/BlogArticle.jsx
+++ components/BlogArticle.jsx
@@ -9,7 +9,7 @@ export default function BlogArticle({article}) {
     <>
       <article
         className="py-8 px-3 prose dark:prose-invert mx-auto min-w-[90%] md:min-w-[80%] xl:min-w-[60%]">
-        <h1 className="my-1.5 text-5xl mt-0">{article.title}</h1>
+        <h1 className="my-1.5 text-4xl mt-0">{article.title}</h1>
         <p className="my-1 text-[#999CA1]">Matt D'Agostino – {article.date} – ☕ {article.readingTimeText}</p>
         <div>
           <MDXRemote {...article.content} components={components}/>
